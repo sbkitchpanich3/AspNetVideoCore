7d77be2fcab185d8db22ae1babd38d0d95e5e9e2
Sikharin Kitchpanich <bankster007@gmail.com>
2018-12-13 14:05:45 -0900

    Add project files.

---
+++ AspNetCoreVideo/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreVideo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreVideo
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddSingleton(provider => Configuration);
+            services.AddMvc();
+            services.AddSingleton<IMessageService, ConfigurationMessageService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageService msg)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc(routes =>
+           {
+               routes.MapRoute(
+                   name: "default",
+                   template: "{controller=Home}/{action=Index}/{id?}");
+           });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync(msg.GetMessage());
+            });
+        }
+    }
+}
